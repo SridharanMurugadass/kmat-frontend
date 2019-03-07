c36f57e978f08b27cb7b3a0a23dc48061f3023ad
Sridharan Murugadass <smurugadass@gogoair.com>
2019-03-07 21:30:57 +0530

    first commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { KonguService } from '../../../app/_services/kongu.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from '../../_services/alert.service';
+
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  //styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public loginForm: FormGroup;
+  public forgotPasswordForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  
+
+
+  constructor(
+    private _service: KonguService,
+    private _activateRoute: ActivatedRoute,
+    private _router: Router,
+    private formBuilder: FormBuilder,
+    private alertService: AlertService) { }
+
+    private homeUrl = 'https://kmat.herokuapp.com/home#login-popup';
+
+  ngOnInit() {
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this._activateRoute.snapshot.queryParams['returnUrl'] || '/profile';
+
+    this.loginForm = this.formBuilder.group({
+      emailMobile: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', Validators.required]
+    });
+
+    $(document).ready(function () {
+      $('#forgotPWFrm').hide();
+
+      $('#forgot').click(function () {
+        $('#loginFrm').hide();
+        $('#forgotPWFrm').show();
+        // alert('test');
+      });
+    });
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+    this.submitted = true;
+    // console.log(this.registerForm.value);
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this._service.login(this.loginForm.value.emailMobile, this.loginForm.value.password).subscribe(
+      userData => {
+
+        
+        console.log('Login successful :: ', userData);
+       
+
+        if(userData != null){
+
+        this._router.navigate([this.returnUrl]);
+        }else{
+          //this._router.navigate([this.serviceURL]);
+
+          console.log('Login failure :: ', userData);
+          
+        }
+
+        localStorage.setItem('currentUser', JSON.stringify(userData));
+        
+        // this.alertService.success('Registration successful', true);
+       
+      },
+     
+      error => {
+        this.alertService.error(error);
+        this.loading = false;
+      }
+    );
+  }
+
+  close() {
+    $('#forgotPWFrm').hide();
+    $('#loginFrm').show();
+    this.loginForm = this.formBuilder.group({
+      userid: '',
+      password: ''
+    });
+  }
+
+}
---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/User';
+import * as $ from 'jquery';
+
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  //styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  currentUser: User;
+
+  constructor() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    
+  }
+
+  ngOnInit() {
+
+
+  }
+
+}
